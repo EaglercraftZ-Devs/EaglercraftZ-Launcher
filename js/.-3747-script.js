510a9baa485a44aa7e05feaa680e0b384f98f963
XxZixiaxX <zixiachen10@gmail.com>
2024-12-31 11:26:31 -0500

    Delete js/.-3747-script.js

--- js/.-3747-script.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the button element
-const button = document.getElementById("playButton"); // Replace "playButton" with the actual ID of your button
-
-// Add an event listener for the click event
-button.addEventListener("click", function() {
-  // This function will be executed when the button is clicked
-  alert("The button was clicked"); // Replace this with your desired action (e.g., redirect to a new page, show/hide content)
-});
